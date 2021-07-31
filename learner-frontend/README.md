b8b368eca2f40f93f70d79b05126ebc4824195d7
Robert Nickel <robert.nickel@aikq.de>
2021-07-31 14:32:19 +0200

    basic setup of learner-frontend

---
+++ learner-frontend/README.md
@@ -0,0 +1,6 @@
+# Learner Frontend
+Create and solve challenges!
+
+## Local execution
+1. `yarn`
+2. `yarn dev`
---
+++ learner-frontend/src/App.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Valexam</h1>
+  <p>Create and solve challenges!</p>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>

9fe6061ba48183e8d355dc8a8d381f348b4d4c36
Robert Nickel <robert.nickel@aikq.de>
2021-09-12 13:53:22 +0200

    Changed the background and font of dummy UI.

--- learner-frontend/src/App.svelte
+++ learner-frontend/src/App.svelte
@@ -8,8 +8,9 @@
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: "Montserrat", sans-serif;
+    background-color: #001733;
+    color: white;
   }
 
   main {
@@ -19,10 +20,8 @@
   }
 
   h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
     font-size: 4rem;
-    font-weight: 100;
+    font-weight: 400;
     line-height: 1.1;
     margin: 2rem auto;
     max-width: 14rem;

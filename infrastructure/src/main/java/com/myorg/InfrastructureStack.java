33a9dbc7b1e82c4d863d94ef412ddc37badd053f
Robert Nickel <github@robertnickel.online>
2021-11-09 17:41:37 +0100

    Added APIGateway to Infrastructure Stack, renamed some-lambda to commitOpenQuestion. Is deployable and callable now.

--- infrastructure/src/main/java/com/myorg/InfrastructureStack.java
+++ infrastructure/src/main/java/com/myorg/InfrastructureStack.java
@@ -1,9 +1,16 @@
 package com.myorg;
 
+import java.util.Arrays;
+
+import software.amazon.awscdk.core.CfnOutput;
 import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.Duration;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.apigatewayv2.AddRoutesOptions;
+import software.amazon.awscdk.services.apigatewayv2.HttpApi;
+import software.amazon.awscdk.services.apigatewayv2.HttpMethod;
+import software.amazon.awscdk.services.apigatewayv2.LambdaProxyIntegration;
 import software.amazon.awscdk.services.lambda.Code;
 import software.amazon.awscdk.services.lambda.Function;
 import software.amazon.awscdk.services.lambda.Runtime;
@@ -16,12 +23,17 @@ public class InfrastructureStack extends Stack {
     public InfrastructureStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        final Function someLambda = Function.Builder
-        .create(this, "some-lambda")
-        .runtime(Runtime.JAVA_11)
-        .timeout(Duration.seconds(30))
-        .code(Code.fromAsset("../some-lambda/target/scala-3.0.1/lambda-scala-seed.jar"))
-        .handler("handler.Handler::handle")
-        .build();
+        final Function commitOpenQuestionLambda = Function.Builder.create(this, "commitOpenQuestion")
+                .runtime(Runtime.JAVA_11).timeout(Duration.seconds(30))
+                .code(Code.fromAsset("../commitOpenQuestionLambda/target/scala-3.0.1/lambda-scala-seed.jar"))
+                .handler("handler.Handler::handle").build();
+
+        final HttpApi httpApi = HttpApi.Builder.create(this, "scalexam").build();
+
+        httpApi.addRoutes(AddRoutesOptions.builder().path("/commitOpenQuestion").methods(Arrays.asList(HttpMethod.GET))
+                .integration(LambdaProxyIntegration.Builder.create().handler(commitOpenQuestionLambda).build())
+                .build());
+
+        CfnOutput.Builder.create(this, "URL").value(httpApi.getUrl() + "commitOpenQuestion").build();
     }
 }

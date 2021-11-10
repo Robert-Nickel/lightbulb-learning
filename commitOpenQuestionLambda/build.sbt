cf72a0a17eee4037ea0c57346d41e33a3f75e561
Kevin Olomu <kevin-olomu@web.de>
2021-11-10 13:34:14 +0100

    fetch information from dynamoDB from lambda function

--- commitOpenQuestionLambda/build.sbt
+++ commitOpenQuestionLambda/build.sbt
@@ -9,10 +9,11 @@ lazy val root = project
     libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test",
     libraryDependencies ++= Seq(
       "com.amazonaws" % "aws-lambda-java-core" % "1.2.1",
-      "com.amazonaws" % "aws-lambda-java-events" % "3.9.0"
+      "com.amazonaws" % "aws-lambda-java-events" % "3.9.0",
+      "com.amazonaws" % "aws-java-sdk-dynamodb" % "1.12.106",
+      "com.github.losizm" %% "little-json" % "9.0.0"
     )
   )
-
 assemblyJarName in assembly := "lambda-scala-seed.jar"
 
 assemblyMergeStrategy in assembly := {
--- commitOpenQuestionLambda/src/main/scala/handler/Handler.scala
+++ commitOpenQuestionLambda/src/main/scala/handler/Handler.scala
@@ -2,16 +2,46 @@ package handler
 
 import java.util.HashMap;
 import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.events.{
   APIGatewayV2HTTPEvent,
   APIGatewayV2HTTPResponse
 }
+import little.json.*
+import little.json.Implicits.{ *, given }
+import scala.language.implicitConversions
+
+case class Body(id: String)
+
+given jsonToBody: JsonInput[Body] with 
+  def apply(json: JsonValue) = Body(json("id"))
 
 class Handler {
+
   def handle(
       apiGatewayEvent: APIGatewayV2HTTPEvent,
       context: Context
   ): APIGatewayV2HTTPResponse = {
+
+    val parsed_body = Json.parse(apiGatewayEvent.getBody())
+    val body = parsed_body.as[Body]
+
+    println(s"body= ${body}")
+
+    val id  = body.id
+    
+    println(s"id = ${id}")
+
+    val client: AmazonDynamoDB = AmazonDynamoDBClientBuilder.standard().build();
+    
+    val dynamoDB: DynamoDB = new DynamoDB(client);
+    val table: Table = dynamoDB.getTable("OpenQuestionDraft-xiukk7jyfnexpd66hqxfnnrmem-prod");
+
+    val item: Item = new Item().withPrimaryKey("Id", id)
 
     val headers = new HashMap[String, String]()
     headers.put("Access-Control-Allow-Headers", "Content-Type")

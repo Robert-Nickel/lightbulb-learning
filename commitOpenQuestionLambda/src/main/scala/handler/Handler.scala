d9b6ece44819ced6940133d15a309b4338adf462
Kevin Olomu <kevin-olomu@web.de>
2021-11-10 11:22:22 +0100

    added cors headers for lambda function

--- commitOpenQuestionLambda/src/main/scala/handler/Handler.scala
+++ commitOpenQuestionLambda/src/main/scala/handler/Handler.scala
@@ -1,5 +1,6 @@
 package handler
 
+import java.util.HashMap;
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.{
   APIGatewayV2HTTPEvent,
@@ -11,10 +12,16 @@ class Handler {
       apiGatewayEvent: APIGatewayV2HTTPEvent,
       context: Context
   ): APIGatewayV2HTTPResponse = {
+    HashMap<String, String> headers = new HashMap<>();
+    headers.put("Content-Type", "application/json");
+    headers.put("Access-Control-Allow-Origin", "*");
+    headers.put("Access-Control-Allow-Methods", "*");
+
     println(s"body = ${apiGatewayEvent.getBody()}")
     APIGatewayV2HTTPResponse
       .builder()
       .withStatusCode(200)
+      .withHeaders(headers)
       .withBody("okay")
       .build()
   }

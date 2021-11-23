779cf75bb26b9690c31531e19ee3af96e5447e0d
Kevin Olomu <kevin-olomu@web.de>
2021-11-23 11:22:59 +0100

    amplify cdk stack

---
+++ app/src/aws-exports.js
@@ -0,0 +1,30 @@
+
+const awsmobile = {
+    "aws_project_region": "eu-central-1",
+    "aws_cognito_identity_pool_id": "eu-central-1:341edf4e-fa32-4627-a4d8-a9fbd191fddc",
+    "aws_cognito_region": "eu-central-1",
+    "aws_user_pools_id": "eu-central-1_390PS6Lsx",
+    "aws_user_pools_web_client_id": "125gjfskdeudh2puk6215mhfsi",
+    "oauth": {},
+    "aws_cognito_login_mechanisms": [
+        "EMAIL"
+    ],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ],
+    "aws_appsync_graphqlEndpoint": "https://flpwalfnqvb7lg5m2blfxpo7au.appsync-api.eu-central-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-central-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-ts3qc3ad7rcdrh536tcc22rfku"
+};
--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,21 +1,69 @@
-import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as path from 'path';
 import { PolicyStatement } from '@aws-cdk/aws-iam';
 import { CorsHttpMethod, HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2';
 import { LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2-integrations';
-import { Duration } from '@aws-cdk/aws-cloudwatch/node_modules/@aws-cdk/core/lib/duration'
 import * as sns from '@aws-cdk/aws-sns';
 import * as sqs from '@aws-cdk/aws-sqs';
 import * as subs from '@aws-cdk/aws-sns-subscriptions';
+import * as cdk from '@aws-cdk/core';
+import * as amplify from '@aws-cdk/aws-amplify';
+import * as codebuild from '@aws-cdk/aws-codebuild';
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // const topic = new sns.Topic(this, 'sns-topic', {
+    //   displayName: 'My SNS topic',
+    // });
+
+    // URLs von API Gateway haben sich geaendert wenn `cdk destroy`
+  
+    const amplifyApp = new amplify.App(this, 'MyApp4', {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: 'Lightbulb-Learning',
+        repository: 'lightbulb-learning',
+        oauthToken: cdk.SecretValue.secretsManager('github-oauth-token')
+      }),
+      buildSpec: codebuild.BuildSpec.fromObjectToYaml({ // Alternatively add a `amplify.yml` to the repo
+        version: '1.0',
+        appRoot: "app",
+        frontend: {
+          phases: {
+            preBuild: {
+              commands: [
+                'yarn'
+              ]
+            },
+            build: {
+              commands: [
+                'yarn build'
+              ]
+            }
+          },
+          artifacts: {
+            baseDirectory: 'public',
+            files:
+            - '**/*'
+          }
+        },
+        // backend: {
+        //   phases: {
+        //     build: {
+        //       commands: ['amplifyPush --simple']
+        //     }
+        //   }
+        // }
+      })
+    });
+
+    // FIXME: add master to main
+    const master = amplifyApp.addBranch('main');
+
     const commitOpenQuestionLambda = new lambda.Function(this, 'commitOpenQuestionLambda', {
       runtime: lambda.Runtime.JAVA_11,
-      timeout: Duration.seconds(30),
+      timeout: cdk.Duration.seconds(30),
       memorySize: 256,
       handler: 'handler.Handler::handle',
       code: lambda.Code.fromAsset(path.join(__dirname, '../../lambdas/commitOpenQuestionLambda/target/scala-3.0.1/lambda-scala-seed.jar')),
@@ -23,7 +71,7 @@ export class InfrastructureStack extends cdk.Stack {
 
     const createOpenQuestionLambda = new lambda.Function(this, 'createOpenQuestionLambda', {
       runtime: lambda.Runtime.JAVA_11,
-      timeout: Duration.seconds(30),
+      timeout: cdk.Duration.seconds(30),
       memorySize: 256,
       handler: 'handler.Handler::handle',
       code: lambda.Code.fromAsset(path.join(__dirname, '../../lambdas/createOpenQuestionLambda/target/scala-3.0.1/lambda-scala-seed.jar')),

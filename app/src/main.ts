9b870dcb366e39be6abeff67dc03ba5c7b113743
Kevin Olomu <kevin-olomu@web.de>
2021-11-23 14:40:00 +0100

    debug (cannot read property Symbol(Symbol.iterator)

--- app/src/main.ts
+++ app/src/main.ts
@@ -4,6 +4,7 @@ import Amplify from '@aws-amplify/core';
 import * as process from 'process';
 const aws_exports = require('./aws-exports').default;
 
+console.log('aws_exports: ', aws_exports);
 Amplify.configure(aws_exports);
 
 // This fix is based on https://github.com/aws-amplify/amplify-js/issues/3274#issuecomment-497773026

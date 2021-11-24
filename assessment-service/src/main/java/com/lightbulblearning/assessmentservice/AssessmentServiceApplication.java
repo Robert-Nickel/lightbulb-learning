7638200c6314562459a7a98812bafd725023e2bf
Robert Nickel <github@robertnickel.online>
2021-11-24 13:33:17 +0100

    Added microservice example.

---
+++ assessment-service/src/main/java/com/lightbulblearning/assessmentservice/AssessmentServiceApplication.java
@@ -0,0 +1,12 @@
+package com.lightbulblearning.assessmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentServiceApplication.class, args);
+	}
+
+}
---
+++ assessment-service/src/test/java/com/lightbulblearning/assessmentservice/AssessmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightbulblearning.assessmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

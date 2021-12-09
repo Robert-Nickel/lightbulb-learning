977b21dd3ab7a98971fd2fbaed82f492e0f5e318
Robert Nickel <github@robertnickel.online>
2021-12-09 14:19:23 +0100

    Revert "Added script to automatically create Amplify environment."
    This reverts commit 7d82ce952adc9874f8cab7559f1b633395a798fb.

--- infrastructure/create_amplify_app.sh
+++
@@ -1,8 +0,0 @@
-mv ../app/amplify/team-provider-info.json ../app/amplify/team-provider-info-old.json 
-cd ../app
-echo "amplify init"
-echo "prod" | amplify init
-# TODO: Add hosting of prod
-echo "amplify publish"
-# amplify publish # TODO: insert y 3x times automatically
-#amplify hosting serve

815c15ae58f7973062e13659144031d278908511
Robert Nickel <github@robertnickel.online>
2021-12-14 09:50:04 +0100

    Wrote some more serious principles.

--- app/src/components/WhatWeDo.svelte
+++ app/src/components/WhatWeDo.svelte
@@ -1,8 +1,23 @@
-<p class="principle">✅  Learning together is more effective than alone.</p>
 <p class="principle">
-    ✅  Online learning means access to knowledge for everyone.
+    💡&nbsp;&nbsp;Our vision is to optimize for the learning itself - because
+    the demands to the graduates of the future are different from those of the
+    past. They need to communicate, collaborate, analyze complex problems and
+    develop solutions in teams. Using access to the internet and knowing how to
+    filter the gigantic amount of information means being able to find solutions
+    for the problems of the future.
 </p>
-<p class="principle">✅  Formative learning by feedback is more valuable than summative assessment.</p>
+
+<p class="principle">
+    🥋&nbsp;&nbsp;Lightbulb Learning lets your students challenge each other by
+    asking good question, answering them precisely and providing feedback for
+    each others work - all of that being a part of their educational
+    performance.
+</p>
+
+<p class="principle">
+    🔥&nbsp;&nbsp;Try Lightbulb Learning for free - so your students can
+    practice today what we all need them to know tomorrow.
+</p>
 
 <style>
     .principle {

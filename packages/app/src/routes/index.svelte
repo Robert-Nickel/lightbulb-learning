4726bdb03c62c3d8996558c6e4279684028953ff
Robert Nickel <github@robertnickel.online>
2021-12-20 18:53:59 +0100

    Revert "created custom route for challenge pools."
    This reverts commit c7fb068d14b6e169f876bf4dd487208070e2cdf7.

--- packages/app/src/routes/index.svelte
+++ packages/app/src/routes/index.svelte
@@ -2,10 +2,11 @@
 	import { store } from '$lib/stores/auth';
 	import ChallengePools from '$lib/components/ChallengePools.svelte';
 	import { SvelteToast, toast } from '@zerodevx/svelte-toast';
+
+	import { Auth } from 'aws-amplify';
 	import StartPage from '$lib/components/StartPage.svelte';
 
 	const baseUrl: string = 'https://yybkc7efv3.execute-api.eu-central-1.amazonaws.com';
-	export let userId: string;
 
 	function showToast(toastEvent) {
 		let theme = {
@@ -16,12 +17,19 @@
 			theme
 		});
 	}
+
+	async function getUserId(): Promise<string> {
+		const user = await Auth.currentAuthenticatedUser();
+		return user.attributes.sub;
+	}
 </script>
 
 {#if $store != null}
-	<main class="container py-4 max-w-screen-sm">
-		<ChallengePools on:toast={showToast} {baseUrl} {userId} />
-	</main>
+	{#await getUserId() then userId}
+		<main class="container py-4 max-w-screen-sm">
+			<ChallengePools on:toast={showToast} {baseUrl} {userId} />
+		</main>
+	{/await}
 {:else}
 	<StartPage />
 {/if}

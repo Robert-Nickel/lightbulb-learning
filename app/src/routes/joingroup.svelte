6abd52ce82c2708d38eea85cb1d7a2e42cfed0fe
Robert Nickel <github@robertnickel.online>
2021-12-21 14:58:45 +0100

    Create joingroup.svelte

---
+++ app/src/routes/joingroup.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Auth } from 'aws-amplify';
+
+	export let baseUrl: string = 'https://yybkc7efv3.execute-api.eu-central-1.amazonaws.com';
+
+	async function addUserToGroup() {
+		var myHeaders = new Headers();
+		myHeaders.append('Content-Type', 'application/json');
+
+		const user = await Auth.currentAuthenticatedUser();
+		const username = user.attributes.email;
+		// TODO: change this hardcoded userpool to be custom for premium instances
+		const userPoolId = 'lightbulblearningapp1ed418f9_userpool_1ed418f9-prod';
+
+		const groupName = document.getElementById('groupName').value;
+		const body = {
+			username,
+			userPoolId,
+			groupName
+		};
+
+		var requestOptions = {
+			method: 'POST',
+			headers: myHeaders,
+			body: JSON.stringify(body)
+		};
+
+		fetch(`${baseUrl}/addUserToGroup`, requestOptions)
+			.then((response) => response.text())
+			.then((result) => console.log(result))
+			.catch((error) => console.log('error', error));
+	}
+</script>
+
+<main class="container pt-4">
+	<input id="groupName" placeholder="Group Name" />
+	<button on:click={addUserToGroup}>Join Group</button>
+</main>

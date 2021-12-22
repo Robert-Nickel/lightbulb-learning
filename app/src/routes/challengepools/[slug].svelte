0631087d0ce91d9d02674b3764af0203d8e0c50c
Robert Nickel <github@robertnickel.online>
2021-12-22 17:11:39 +0100

    Polished and simplified the rendering of open questions.

--- app/src/routes/challengepools/[slug].svelte
+++ app/src/routes/challengepools/[slug].svelte
@@ -1,24 +1,29 @@
 <script lang="ts">
-	import { ChallengePool } from '$lib/models';
+	import { ChallengePool, OpenQuestion } from '$lib/models';
 	import { DataStore } from 'aws-amplify';
 	import OpenQuestionDrafts from '$lib/components/OpenQuestionDrafts.svelte';
-	import OpenQuestions from '$lib/components/OpenQuestions.svelte';
 	import { goto } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
 	import { user } from '$lib/stores/user';
+	import OpenAnswers from '$lib/components/OpenAnswers.svelte';
 
-	let challengePool;
-	let openQuestions;
+	let challengePool: ChallengePool;
+	let openQuestions: Array<OpenQuestion> = [];
 
 	onMount(async () => {
+		refresh()
+	});
+
+	async function refresh() {
 		const poolId = $page.params.slug;
 		try {
 			challengePool = await DataStore.query(ChallengePool, poolId);
+			openQuestions = await DataStore.query(OpenQuestion, (q) => q.challengepoolID('eq', challengePool.id));
 		} catch (error) {
-			throw new Error(`There is no challenge pool with id ${poolId}`);
+			throw error;
 		}
-	});
+	}
 
 	async function deletePool() {
 		await DataStore.delete(await DataStore.query(ChallengePool, challengePool.id));
@@ -26,17 +31,42 @@
 	}
 
 	function openQuestionCommitted() {
-		openQuestions.fetchOpenQuestions();
+		refresh();
 	}
 </script>
 
 <main class="container">
 	{#if challengePool}
 		<h1>{challengePool.description}</h1>
-		{#if $user.id == challengePool.owner}
-			<button on:click={deletePool} class="secondary outline w-auto mb-0">Delete</button>
-		{/if}
+
 		<OpenQuestionDrafts {challengePool} on:toast on:openQuestionCommitted={openQuestionCommitted} />
-		<OpenQuestions bind:this={openQuestions} {challengePool} />
+		{#if openQuestions.length > 0}
+			<h3 class="mt-10">Open Questions</h3>
+		{/if}
+		{#each openQuestions as openQuestion}
+			{#if openQuestion.owner == $user.id}
+				<article class="yours">
+					<i>You asked:</i>
+					{openQuestion.questionText}
+				</article>
+			{:else}
+				<article>
+					<p>{openQuestion.questionText}</p>
+					<OpenAnswers bind:openQuestion />
+				</article>
+			{/if}
+		{/each}
+
+		{#if $user.id == challengePool.owner}
+			<button on:click={deletePool} class="secondary outline w-auto mb-0"
+				>Delete {challengePool.description}</button
+			>
+		{/if}
 	{/if}
 </main>
+
+<style>
+	.yours {
+		border-left: 4px solid var(--primary);
+	}
+</style>

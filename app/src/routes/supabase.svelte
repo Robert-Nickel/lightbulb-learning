b0a6130c8d03f5703a6b88f7df64170747207636
Robert Nickel <github@robertnickel.online>
2022-01-05 14:40:05 +0100

    supabase example request

---
+++ app/src/routes/supabase.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { createClient } from '@supabase/supabase-js';
+	import { onMount } from 'svelte';
+	import type { definitions } from '../../supabase';
+	type challenge_pools2 = definitions['challenge_pools'];
+	const supabaseUrl = 'https://rkpsxesguhibbzhylmsd.supabase.co';
+	const supabaseKey = 'TODO';
+	const supabase = createClient(supabaseUrl, supabaseKey);
+
+	onMount(async () => {
+		//let { data, error } = await supabase.from('challenge_pools').select();
+		const { data, error } = await supabase
+			.from<challenge_pools2>('challenge_pools')
+			.insert([{ description: 'This is the new one then' }]);
+
+		alert(JSON.stringify(data));
+	});
+</script>

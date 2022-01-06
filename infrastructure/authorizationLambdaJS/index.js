39f6850abc326b9553daa5121a4f9a0c0863f723
Kevin Olomu <kevin-olomu@web.de>
2022-01-06 18:23:23 +0100

    update auth function to take in ID token to read admin_of_group property

--- infrastructure/authorizationLambdaJS/index.js
+++ infrastructure/authorizationLambdaJS/index.js
@@ -3,35 +3,36 @@ const jwkToPem = require('jwk-to-pem');
 const axios = require('axios');
 const jwt_decode = require("jwt-decode");
 
-exports.handler =  function(event, context, callback) {
+/** This authorization lambda function is expected to provide an ACCESS token, to validate the request for upgrading a group from free to standard. Only users who have an ADMIN_OF_GROUP property can upgrade this group. */
+exports.handler = function (event, context, callback) {
     axios.get("https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_bAc9VMMys/.well-known/jwks.json").then(
         response => {
-            const jwt_token = event.headers.authorization;
+            console.log(event.headers.authorization);
+            const jwt_token = JSON.parse(event.headers.authorization).jwtToken;
             const decoded_token = jwt_decode(jwt_token, { header: true })
             const valid_jwks = response.data.keys.filter(jwk => jwk['kid'] == decoded_token['kid'])
 
-            // auslesen welcher user es ist und Attribute AdminOfGroup
-            // ID Token enhaelt custom:adminOfGroup
-            // siehe auth.ts -> wir vertrauen ID token, da es jwtToken enthaelt und wir das abgleichen koennen, stimmt es ueberein, nehmen wir an, dass die Anfrage ok ist.
-
-            if(valid_jwks.length !== 1) {
+            if (valid_jwks.length !== 1) {
                 console.error("no valid jwk or multiple jwks found")
                 callback("Error: no valid JWK for JWT found")
             } else {
                 jwk = valid_jwks[0];
-                if(jwt_token.length > 0) {
-                    const decoded = jwt.verify(jwt_token, jwkToPem(jwk))
-                    console.log(decoded)
+                const decoded = jwt.verify(jwt_token, jwkToPem(jwk))
+                console.log(decoded)
+                const admin_of_group = decoded['custom:admin_of_group'];
+                console.log("admin of group", admin_of_group)
+                if (admin_of_group.length > 0) {
                     callback(null, generateSimpleResponse(true));
                 } else {
-                    callback("Error: Invalid JWT token"); // Return a 500 Invalid token 
-                }    
+                    callback("Error: no admin of group");
+                }
             }
+            callback("Error: Invalid JWT token"); // Return a 500 Invalid token     
         }
     )
 };
 
-var generateSimpleResponse = function(isAllowed) {
+const generateSimpleResponse = function (isAllowed) {
     return {
         "isAuthorized": isAllowed
     };

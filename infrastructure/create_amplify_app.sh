d48a9426dbc3f416b6c1b83a5f61c2efdcd8e269
Robert Nickel <github@robertnickel.online>
2022-01-10 17:04:46 +0100

    Create a new branch based on the premium instance

--- infrastructure/create_amplify_app.sh
+++ infrastructure/create_amplify_app.sh
@@ -1,20 +1,18 @@
-# How to use this script:
-# If prompted, answer yes
-
+echo "What is the name of the university"
+read university
 mv ../app/amplify/team-provider-info.json ../app/amplify/team-provider-info-old.json 
 cd ../app
 echo "amplify init"
 echo "prod" | amplify init
-mv aws-exports.js aws-exports.cjs
+cp src/aws-exports.js src/aws-exports.cjs
 echo "amplify re-init"
 amplify init
-# Do you want to use an existing environment? yes
-# Choose the environment you would like to use? prod
-
-echo "amplify publish"
-amplify publish
-echo "amplify hosting serve"
-amplify hosting serve
+echo "amplify push"
+amplify push
+echo "creating branch " + $university
+git branch $university
+git checkout $university
+git push -u origin $university
 
 # Then do this
 
@@ -23,9 +21,3 @@ amplify hosting serve
 # Backend environment
 # Set up Amplify Studio -> On
 # Data -> Save and deploy -> Deploy
-
-# If amplify publish fails:
-# In AWS console go to Amplify -> Hosting environment
-# npm run build and ZIP the contents of /build folder
-# upload the ZIP, define a name for the environment, click on the new domain
-# Done

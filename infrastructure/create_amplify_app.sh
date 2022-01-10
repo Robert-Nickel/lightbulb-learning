a4eea5c1c03a611ba18c0456e173beb33ac67c6a
Robert Nickel <github@robertnickel.online>
2022-01-10 11:45:44 +0100

    amplify env script

--- infrastructure/create_amplify_app.sh
+++ infrastructure/create_amplify_app.sh
@@ -2,7 +2,15 @@ mv ../app/amplify/team-provider-info.json ../app/amplify/team-provider-info-old.
 cd ../app
 echo "amplify init"
 echo "prod" | amplify init
+mv aws_exports.js aws_exports.cjs
+echo "amplify re-init"
+amplify init # use existing environment? - yes which one? - prod
+
 echo "amplify publish"
 amplify publish
 echo "amplify hosting serve"
 amplify hosting serve
+
+
+
+# Set Up Studio?

48d02b2b7032903099831ba8fc99d9e643fa3aca
Kevin Olomu <kevin-olomu@web.de>
2022-01-14 13:06:12 +0100

    jwk dynamic user pool from jwt iss

--- js_lambdas/authorizationLambdaJS/index.js
+++ js_lambdas/authorizationLambdaJS/index.js
@@ -6,12 +6,14 @@ const { CognitoIdentityServiceProvider } = require('aws-sdk')
 
 /** This authorization lambda function is expected to provide an ACCESS token, to validate the request for upgrading a group from free to standard. Only users who have an ADMIN_OF_GROUP property can upgrade this group. */
 exports.handler = function (event, context, callback) {
-    axios.get("https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_ioweDqC3S/.well-known/jwks.json").then(
+    console.log(event.headers.authorization);
+    const jwt_token = JSON.parse(event.headers.authorization).jwtToken;
+    const decoded_token = jwt_decode(jwt_token);
+    const decoded_token_header = jwt_decode(jwt_token, { header: true })
+
+    axios.get(`${decoded_token['iss']}/.well-known/jwks.json`).then(
         response => {
-            console.log(event.headers.authorization);
-            const jwt_token = JSON.parse(event.headers.authorization).jwtToken;
-            const decoded_token = jwt_decode(jwt_token, { header: true })
-            const valid_jwks = response.data.keys.filter(jwk => jwk['kid'] == decoded_token['kid'])
+            const valid_jwks = response.data.keys.filter(jwk => jwk['kid'] == decoded_token_header['kid'])
 
             if (valid_jwks.length !== 1) {
                 console.error("no valid jwk or multiple jwks found")
@@ -20,17 +22,6 @@ exports.handler = function (event, context, callback) {
                 jwk = valid_jwks[0];
                 const decoded = jwt.verify(jwt_token, jwkToPem(jwk))
                 console.log("decoded", decoded)
-
-                // if decoded doesn't contain admin_of_group property...
-                // use fresh identity token ;) or below code...
-                
-                //   var provider =  new aws.CognitoIdentityServiceProvider();
-                //   provider.getUser({
-                //     AccessToken: jwt_token
-                //   }, function(err, data) {
-                //     if (err) console.log(err, err.stack); // an error occurred
-                //     else     console.log(data);           // successful response
-                //   });
                 const admin_of_group = decoded['custom:admin_of_group'];
                 console.log("admin of group", admin_of_group)
                 if (admin_of_group.length > 0) {

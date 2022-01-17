e287dbe1d8052e2cb2b7bba344f534e595b84e12
Robert Nickel <github@robertnickel.online>
2022-01-17 15:04:25 +0100

    Update StartPage.svelte

--- app/src/lib/components/StartPage.svelte
+++ app/src/lib/components/StartPage.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
 	import NewsletterSignUp from '$lib/components/NewsletterSignUp.svelte';
 	import WhatWeDo from '$lib/components/WhatWeDo.svelte';
 </script>
@@ -12,7 +14,7 @@
 <main class="container mx-auto py-4 px-2 max-w-screen-sm">
 	<button
 		on:click={() => {
-			console.log('start clicked');
+			goto('/login');
 		}}
 		class="w-48 m-auto mb-16">Start</button
 	>

17942156ae0cdb266e99836be68fe16bd4324752
Robert Nickel <github@robertnickel.online>
2022-01-23 19:47:13 +0100

    Update help.svelte

--- app/src/routes/help.svelte
+++ app/src/routes/help.svelte
@@ -2,8 +2,8 @@
 
 <div>
 	...might need help some day. If you have questions, problems, wishes, ideas, job offerings, an opinion or
-	want to delete your data, invest your time or money or talk to someone because it has been a while (we are
-	all in this together), please contact me - I am super nice. <br /><br />
+	want to delete your data, invest your ressources or talk to someone because it's been a while, please
+	contact me - I am super nice. <br /><br />
 	<p>Discord: <kbd>Robert Nickel#6909</kbd></p>
-    <p>E-Mail: <kbd>lightbulb@robertnickel.online</kbd></p>
+	<p>E-Mail: <kbd>lightbulb@robertnickel.online</kbd></p>
 </div>

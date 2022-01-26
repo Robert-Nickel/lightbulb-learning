b83a7851241f645988370d5130a73f7ffaa747a0
Robert Nickel <github@robertnickel.online>
2022-01-26 15:53:36 +0100

    Create create_profiles_table.sql

---
+++ app/supabase/create_profiles_table.sql
@@ -0,0 +1,12 @@
+create table if not exists public.profiles (
+    id uuid not null primary key DEFAULT uuid_generate_v4(),
+    first_name text not null,
+    last_name text not null,
+    university uuid references public.universities on delete cascade not null,
+    user_id uuid references auth.users on delete cascade not null,
+    created_at timestamp with time zone default now() not null
+);
+ALTER TABLE profiles ENABLE ROW LEVEL SECURITY;
+CREATE POLICY "p_insert_policy_for_authenticated_user" ON public.profiles FOR INSERT WITH CHECK (auth.role() = 'authenticated');
+CREATE POLICY "p_select_policy_for_owner" ON public.profiles FOR SELECT USING (auth.uid() = user_id);
+CREATE POLICY "p_update_policy_for_owner" ON public.profiles FOR UPDATE USING (auth.uid() = user_id);

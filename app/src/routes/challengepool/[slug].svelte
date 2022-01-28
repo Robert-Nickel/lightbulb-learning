eb4e12d41b1c520493a1282e359642456597420a
Robert Nickel <github@robertnickel.online>
2022-01-28 18:14:20 +0100

    Introduced navbar inside the challenge pool (currently for the owner only).

--- app/src/routes/challengepool/[slug].svelte
+++ app/src/routes/challengepool/[slug].svelte
@@ -14,9 +14,13 @@
 	} from '$lib/supabaseClient';
 	import { user } from '$lib/stores/user';
 
+	const openQuestionsString = 'open_questions';
+	const settingsString = 'settings';
+
 	let challengePool: ChallengePoolType;
 	let openQuestions: Array<OpenQuestionType> = [];
 	let inviteCode: string;
+	let currentTab = 'open_questions';
 
 	onMount(() => {
 		refresh();
@@ -33,54 +37,59 @@
 	{#if challengePool}
 		<h1>{challengePool.description}</h1>
 
-		<CreateOpenQuestion {challengePool} on:openQuestionCommitted={refresh} />
-
-		{#if openQuestions.length > 0}
-			<h3 class="mt-10">Open Questions</h3>
-		{/if}
-		{#each openQuestions as openQuestion}
-			<a href={`/openquestion/${openQuestion.id}`} class="light-link">
-				{#if openQuestion.owner == $user.id}
-					<article class="yours hoverable">
-						<i>You asked:</i>
-						{openQuestion.questionText}
-					</article>
-				{:else}
-					<article class="hoverable">
-						{openQuestion.questionText}
-					</article>
-				{/if}
-			</a>
-		{/each}
-
 		{#if $user.id == challengePool.owner}
-			<div class="flex space-x-2">
-				<button
-					on:click={async () => {
-						const randomTenCharString = Math.random().toString(16).substring(2, 12);
-						inviteCode = (await saveInviteCode(challengePool.id, randomTenCharString)).code;
-						navigator.clipboard.writeText(inviteCode);
+			<header class="flex p-2 space-x-4 border-b-2 ">
+				<nav
+					class={currentTab === openQuestionsString ? 'activeNavElement' : ''}
+					on:click={() => {
+						currentTab = openQuestionsString;
 					}}
-					class="secondary outline w-auto mb-0"
-					>Generate Invite Code
-				</button>
-
-				<button
-					on:click={async () => {
-						if (
-							confirm(
-								'Everything within ' + challengePool.description + ' will be gone forever.\nAre you really sure?'
-							)
-						) {
-							await deleteChallengePool(challengePool.id);
-							goto('/');
-						}
-					}}
-					class="secondary outline w-auto mb-0 hover-red">Delete {challengePool.description}</button
 				>
-			</div>
+					Open Questions
+				</nav>
+				<nav
+					class={currentTab === settingsString ? 'activeNavElement' : ''}
+					on:click={() => {
+						currentTab = settingsString;
+					}}
+				>
+					Settings
+				</nav>
+			</header>
+		{/if}
+
+		{#if currentTab == openQuestionsString}
+			<CreateOpenQuestion {challengePool} on:openQuestionCommitted={refresh} />
+
+			{#if openQuestions.length > 0}
+				<h3 class="mt-10">Open Questions</h3>
+			{/if}
+			{#each openQuestions as openQuestion}
+				<a href={`/openquestion/${openQuestion.id}`} class="light-link">
+					{#if openQuestion.owner == $user.id}
+						<article class="yours hoverable">
+							<i>You asked:</i>
+							{openQuestion.questionText}
+						</article>
+					{:else}
+						<article class="hover:">
+							{openQuestion.questionText}
+						</article>
+					{/if}
+				</a>
+			{/each}
+		{:else if currentTab == settingsString}
+			<button
+				on:click={async () => {
+					const randomTenCharString = Math.random().toString(16).substring(2, 12);
+					inviteCode = (await saveInviteCode(challengePool.id, randomTenCharString)).code;
+					navigator.clipboard.writeText(inviteCode);
+				}}
+				class="secondary outline w-auto my-4"
+				>Generate Invite Code
+			</button>
 			{#if inviteCode}
-				<div class="mt-3">
+				<div class="mb-4">
 					Invite Code: <a
 						on:click={() => {
 							navigator.clipboard.writeText(inviteCode);
@@ -89,6 +98,22 @@
 					> (valid: 7 days)
 				</div>
 			{/if}
+
+			<button
+				on:click={async () => {
+					if (
+						confirm(
+							'Everything within ' +
+								challengePool.description +
+								' will be gone forever.\nAre you really sure?'
+						)
+					) {
+						await deleteChallengePool(challengePool.id);
+						goto('/');
+					}
+				}}
+				class="secondary outline w-auto mb-0 hover-red">Delete {challengePool.description}</button
+			>
 		{/if}
 	{/if}
 
@@ -102,5 +127,25 @@
 
 	.hoverable:hover {
 		background-color: var(--card-sectionning-background-color);
+	}
+
+	header {
+		border-color: var(--card-sectionning-background-color);
+	}
+
+	nav {
+		color: var(--secondary);
+	}
+
+	nav:hover {
+		color: var(--white);
+		font-weight: 500;
+		border-color: var(--primary);
+	}
+
+	.activeNavElement {
+		color: var(--white);
+		font-weight: 500;
+		border-color: var(--primary);
 	}
 </style>

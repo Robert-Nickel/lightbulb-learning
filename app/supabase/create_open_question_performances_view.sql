db9cc64c6c8432b9ae568af57ade5a217196f327
Robert Nickel <github@robertnickel.online>
2022-01-31 20:27:10 +0100

    Update create_open_question_performances_view.sql

--- app/supabase/create_open_question_performances_view.sql
+++ app/supabase/create_open_question_performances_view.sql
@@ -7,4 +7,4 @@ select
 from challenge_pool_user
 inner join open_questions on open_questions.owner = challenge_pool_user.user_id 
 inner join correct_open_answers on correct_open_answers.open_question = open_questions.id
-where user_id = open_question_id;
+where user_id = open_questions.id;

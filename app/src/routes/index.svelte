44ded429c3fac633648c1308a7a8a7d16fc200d1
samuba <samuel.z.bach@gmail.com>
2022-02-13 11:39:54 +0100

    fix login

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,26 +1,36 @@
 <script lang="ts" context="module">
-	import { fetchProfile } from '$lib/supabaseClient';
 	import type { Load } from '@sveltejs/kit';
 	import type { Session } from '@supabase/supabase-js';
 
 	export const load: Load = async ({ session }) => {
 		const { user } = session as Session;
-		if (!user) return {};
-
-		const profile = await fetchProfile(user.id);
-		if (user && !profile) {
-			return { status: 302, redirect: '/welcome' };
+		if (user) {
+			return { status: 302, redirect: '/evaluateAuth' };
 		}
-		if (user && profile) {
-			return { status: 302, redirect: '/challengepool' };
-		}
+		return {};
 	};
 </script>
 
 <script lang="ts">
 	import StartPage from '$lib/components/StartPage.svelte';
+	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
+
+	let loginInProgress = $page.url.hash.includes('#access_token=');
+
+	onMount(() => {
+		if (loginInProgress) {
+			// this was a redirect from the magic link.
+			// By the time this line executes the supabase client did not yet write the token to localstorage therefore we need to defer the auth evaluation call a little bit
+			setTimeout(() => location.replace(`${location.origin}/evaluateAuth`), 700);
+		}
+	});
 </script>
 
-<StartPage />
+{#if loginInProgress}
+	<h3 class="text-center" aria-busy="true">logging in...</h3>
+{:else}
+	<StartPage />
+{/if}
 
 <!--Go here to see which toasts are possible: https://zerodevx.github.io/svelte-toast/-->

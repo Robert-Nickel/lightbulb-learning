dd880bf0b7659ea6dc2164aed67e8e8e0e05e3bb
Robert Nickel <github@robertnickel.online>
2022-02-25 19:44:55 +0100

    Added GitHub Login to enable local development.

--- app/src/routes/login.svelte
+++ app/src/routes/login.svelte
@@ -18,19 +18,32 @@
 </script>
 
 <main class="container flex justify-center">
-		<article>
-			<header>
-				<h3 class="text-3xl mb-0">Login</h3>
-			</header>
-			<form on:submit|preventDefault={handleLogin}>
+	<article>
+		<header>
+			<h3 class="text-3xl mb-0">Login</h3>
+		</header>
+		<form on:submit|preventDefault={handleLogin}>
+			<div>
+				<p class="description">Login via magic link</p>
 				<div>
-					<p class="description">Login via magic link</p>
-					<div>
-						<input type="email" placeholder="learn@everyday.org" bind:value={email} />
-						<input type="submit" value={'Send magic link'} disabled={loading || !email} />
-					</div>
-					{#if message}<b>{message}</b>{/if}
+					<input type="email" placeholder="learn@everyday.org" bind:value={email} />
+					<input type="submit" value={'Send magic link'} disabled={loading || !email} />
 				</div>
-			</form>
-		</article>
+				{#if message}<b>{message}</b>{/if}
+			</div>
+		</form>
+		<button
+			class="outline"
+			on:click={async () => {
+				loading = true;
+				const { error } = await user.signInGitHub();
+				if (error) {
+					message = error.message;
+				} else {
+					message = 'You are redirected to GitHub login!';
+				}
+				loading = false;
+			}}>Sign in with GitHub</button
+		>
+	</article>
 </main>

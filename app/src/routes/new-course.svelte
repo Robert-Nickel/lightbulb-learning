f1b0c816633abc0e16c4d122a205e51f1572d9e6
Robert Nickel <github@robertnickel.online>
2022-03-28 11:07:12 +0200

    Simplified New Course

--- app/src/routes/new-course.svelte
+++ app/src/routes/new-course.svelte
@@ -7,24 +7,13 @@
 </script>
 
 <h1>New Course</h1>
-<div class="space-y-4">
-	<div class="flex justify-between space-x-2">
-		<div class="w-full">
-			<input
-				bind:value={createCourseDescription}
-				class="w-full"
-				placeholder="Create new Course"
-			/>
-		</div>
-		<div>
-			<button
-				on:click={async () => {
-					const course = await saveCourse(createCourseDescription);
-					createCourseDescription = '';
-					goto(routes.course(course.id));
-				}}
-				class="w-32">Create</button
-			>
-		</div>
-	</div>
-</div>
+
+<input bind:value={createCourseDescription} class="w-full" placeholder="Course Name" />
+<button
+	on:click={async () => {
+		const course = await saveCourse(createCourseDescription);
+		createCourseDescription = '';
+		goto(routes.course(course.id));
+	}}
+	class="w-32">Create</button
+>

6a589d7b408d9480337ca6b2c26c41eafa96f734
Robert Nickel <github@robertnickel.online>
2022-03-28 10:35:42 +0200

    Added login button to navbar.

--- app/src/lib/components/Navbar.svelte
+++ app/src/lib/components/Navbar.svelte
@@ -1,14 +1,36 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { routes } from '$lib/routes';
 	import Hamburger from './Hamburger.svelte';
+	import { user } from '$lib/stores/user';
+
 	export let sidebar = false;
 </script>
 
-<header class="flex p-2 items-center text-white">
-	<nav class="flex">
-		<Hamburger bind:open={sidebar} />
-	</nav>
-	<nav><a href={routes.root} class="text-white mb-0.5" sveltekit:prefetch>Lightbulb Learning</a></nav>
+<header class="flex p-2 items-center text-white justify-between">
+	<div class="flex">
+		<nav class="flex">
+			<Hamburger bind:open={sidebar} />
+		</nav>
+		<nav>
+			<a
+				href={routes.root}
+				class="text-white	"
+				style="margin-top:0em; margin-bottom: 0.8em;"
+				sveltekit:prefetch>Lightbulb Learning</a
+			>
+		</nav>
+	</div>
+	{#if !$user}
+		<nav>
+			<button
+				class="outline"
+				on:click={() => {
+					goto(routes.login);
+				}}>Login</button
+			>
+		</nav>
+	{/if}
 </header>
 
 <!--

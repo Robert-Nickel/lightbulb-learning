8ab6a6c281a6ccf04325ce346e88d7c885c2ecd9
Robert Nickel <github@robertnickel.online>
2022-04-05 17:09:26 +0200

    Added feedback to FAQ

--- app/src/lib/components/FAQ.svelte
+++ app/src/lib/components/FAQ.svelte
@@ -29,3 +29,19 @@
 		href="https://www.indeed.com/career-advice/career-development/what-makes-good-question">Indeed</a
 	>
 </small>
+
+<h5>How to give constructive feedback?</h5>
+<iframe
+	width="560"
+	height="315"
+	src="https://www.youtube-nocookie.com/embed/4IG751YvPYw"
+	title="YouTube video player"
+	frameborder="0"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	allowfullscreen
+/>
+<small
+	>Length: 3:26 - Inspired by <a
+		href="https://www.coursera.support/s/article/209818803-Write-peer-reviews?language=en_US">Coursera</a
+	>
+</small>

b90d90ba26245511ee80e233925f974af694d6c8
Robert Nickel <github@robertnickel.online>
2022-07-20 10:47:25 +0200

    forward to courses if user exists on root index

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
 	import StartPage from '$lib/components/StartPage.svelte';
+	import { session } from '$app/stores';
+	import { goto } from '$app/navigation';
+	import { routes } from '$lib/routes';
+
+	$: {
+		if ($session.user && $session.user.id) {
+			console.log('$session.user exists -> goto courses');
+			goto(routes.courses);
+		}
+	}
 </script>
 
 <StartPage />

453ccd1d8b005d54d7eec9b2fe3051bac5275db0
Robert Nickel <github@robertnickel.online>
2022-07-21 11:59:25 +0200

    Update login.svelte

--- app/src/routes/login.svelte
+++ app/src/routes/login.svelte
@@ -3,16 +3,13 @@
 	import { supabaseClient } from '$lib/db';
 	import { session } from '$app/stores';
 	import { routes } from '$lib/routes';
-	import { goto } from '$app/navigation';
-
-	$: {
-		if ($session.user && $session.user.id) {
-			goto(routes.courses);
-		}
-	}
 </script>
 
 <main class="container">
 	<h1>Login</h1>
-	<Auth {supabaseClient} providers={['GitHub']} redirectAfterLogin={routes.courses} />
+	{#if $session.user && $session.user.id}
+		<p>Success!</p>
+	{:else}
+		<Auth {supabaseClient} providers={['GitHub']} redirectAfterLogin={routes.courses} />
+	{/if}
 </main>
